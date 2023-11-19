99ddf6dd162465d26cc36576672853808876b061
usamaZia <usamazia@addo.ai>
2023-11-19 23:51:32 +0500

    login-logout-basic-setup-with-redux

---
+++ src/store/authSlice.js
@@ -0,0 +1,66 @@
+
+import { createSlice  } from "@reduxjs/toolkit";
+
+
+
+const initialState = {
+    status:false,
+    userData:null,
+}
+
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers:{
+        
+        login:(state, action) => {
+            console.log("redux login");
+            
+            state.status = true;
+            state.userData = action.payload.userData    
+        },
+
+        logout: (state, action)=>{
+            console.log("redux logout");
+
+            state.status = false;
+            status.userData = null;
+        }
+
+    }
+})
+
+
+export const { login, logout } = authSlice.actions;
+
+
+export default authSlice.reducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
